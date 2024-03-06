c4aafbfc5914fddd2c277b1495914e2a3b7e8e37
Cosmo Hidalgo <bjlproduces@gmail.com>
2024-03-05 21:58:38 -0600

    add dotenv-expand to vite.config for vercel/postgres

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenvExpand from 'dotenv-expand'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	// server: {
-	// 	port: 5173,
-	// 	proxy: {
-	// 		'/api': {
-	// 			target: 'http://localhost:3000',
-	// 			changeOrigin: true,
-	// 			secure: false,
-	// 		},
-	// 	},
-	// },
+export default defineConfig(({ mode }) => {
+	// Only modify process.env in development mode
+	if (mode === 'development') {
+		const env = loadEnv(mode, process.cwd(), '')
+		dotenvExpand.expand({ parsed: env })
+	}
+
+	return {
+		plugins: [react()],
+		// server: {
+		// 	port: 5173,
+		// 	proxy: {
+		// 		'/api': {
+		// 			target: 'http://localhost:3000',
+		// 			changeOrigin: true,
+		// 			secure: false,
+		// 		},
+		// 	},
+		// },
+	}
 })
